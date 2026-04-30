da785d624076fff80d9c0f701f8c3f8cc4115ef2
TAHA MAHMOUD <tahamahmoudai@gmail.com>
2026-04-30 13:41:13 +0300

    MAS-9: redirect users based on role Doctor → /Doctor  dashboard & Admin → /Admin dashboard

--- app/(pages)/dashboard/@doctor/page.tsx
+++ app/(pages)/dashboard/@doctor/page.tsx
@@ -1,7 +1,7 @@
 
 const page = () => {
   return (
-    <div>Dashboard</div>
+    <div>Doctor Dashboard</div>
   )
 }
 
---
+++ app/(pages)/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import { checkUserRole } from "@/lib/auth/checkUserRole";
+import { UserRole } from "@/types/user-role";
+
+const layout = async ({
+  doctor,
+  admin,
+}: LayoutProps<"/dashboard">) => {
+  const userRole = await checkUserRole();
+  console.log(userRole);
+
+  return (
+      <div className="bg-red-600">
+          
+      {userRole == UserRole.Doctor ? doctor : admin}
+    </div>
+  );
+};
+
+export default layout;
